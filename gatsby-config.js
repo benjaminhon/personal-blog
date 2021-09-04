cf4972e6bbe70fa90ae6af8d8eeb053e26f61e2d
benjamin <benjaminhon86@gmail.com>
2021-09-04 12:14:14 +0800

    Move to github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "blog",
   },
+  pathPrefix: "/personal-blog", // hosted on https://benjaminhon.github.io/personal-blog/
   plugins: [
     'gatsby-plugin-react-helmet',
     {
